fae465097db992ac80a54d91c9d2e269d27e1b97
pawan-vaishnav <pawandasbairagi07196@gmail.com>
2026-06-16 11:06:25 +0530

    Second

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 // db.js
 import mongoose from 'mongoose';
-
+import dns from 'node:dns';
 const connectDB = async () => {
   try {
+    dns.setServers(['1.1.1.1','8.8.8.8']);
     const conn = await mongoose.connect(process.env.MONGO_URI, {
 
     });
